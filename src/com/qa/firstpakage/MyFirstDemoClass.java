3c46283d94c1b71c18abd42f153da97c844ac39f
RajputAtgithub <surajsinghrajput51@gmail.com>
2017-12-31 19:58:40 +0530

    added minor Changes

---
+++ src/com/qa/firstpakage/MyFirstDemoClass.java
@@ -0,0 +1,32 @@
+package com.qa.firstpakage;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author suraj rajput.
+ *         Created Dec 31, 2017.
+ */
+public class MyFirstDemoClass {
+
+
+
+/**
+ * TODO Put here a description of what this constructor does.
+ *
+ */
+public MyFirstDemoClass() {
+	// TODO Auto-generated constructor stub.
+}
+
+
+@SuppressWarnings("unused")
+private static void MyFirstDemoMethod() {
+
+System.out.println("MyFirstDemoMethod");
+
+
+}
+
+}
+
+
