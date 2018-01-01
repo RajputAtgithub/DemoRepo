4df3eb398374926e2d0e35c4ea9d375c3797f5af
RajputAtgithub <surajsinghrajput51@gmail.com>
2018-01-01 12:00:58 +0530

    this is my firsh commit

---
+++ src/com/qa/secondpakage/MySecondClass.java
@@ -0,0 +1,22 @@
+package com.qa.secondpakage;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author suraj rajput.
+ *         Created Jan 1, 2018.
+ */
+public class MySecondClass {
+	/**
+	 * TODO Put here a description of what this constructor does.
+	 *
+	 */
+	public MySecondClass() {
+		// TODO Auto-generated constructor stub.
+		System.out.println("My Second Class constructor");
+		
+	}
+	
+
+
+}
